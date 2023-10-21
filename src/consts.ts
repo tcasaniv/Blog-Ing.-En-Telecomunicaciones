6f2570cfb6a916af2c4824e8c7d5a52ca19da76e
Terry Casani Valencia <99104321+tcasaniv@users.noreply.github.com>
2023-10-21 15:27:15 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const BRAND = "Acme";
-export const SITE_TITLE = "Astro Engineering Blog";
-export const SITE_URL = "https://astro-engineering-blog.netlify.app";
-export const SITE_DESCRIPTION = "Welcome to our Engineering blog!";
+export const BRAND = "EPIT Blog";
+export const SITE_TITLE = "EPIT Blog";
+//export const SITE_URL = "https://astro-engineering-blog.netlify.app";
+// Desplegado en Vercel
+export const SITE_DESCRIPTION = "Un blog sobre todo lo relacionado a Ingeniería En Telecomunicaciones.";
 export const PAGINATION_POSTS_PER_PAGE = 5;
